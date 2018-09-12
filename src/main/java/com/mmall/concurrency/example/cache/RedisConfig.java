4fd51788ba0a822329d68fe53a2512b96a21947d
chao2015 <136788909@qq.com>
2018-09-12 19:31:21 +0800

    cache and rateLimiter

---
+++ src/main/java/com/mmall/concurrency/example/cache/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.mmall.concurrency.example.cache;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Created by chao on 2018/9/12.
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Bean(name = "redisPool")
+    public JedisPool jedisPool(@Value("${jedis.host}") String host,
+                               @Value("${jedis.port} ") int port) {
+        return new JedisPool(host, port);
+    }
+}
---
+++ src/main/java/com/mmall/concurrency/example/rateLimiter/RateLimiterExample1.java
@@ -0,0 +1,32 @@
+package com.mmall.concurrency.example.rateLimiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by chao on 2018/9/12.
+ *
+ * Google开源工具包Guava提供了限流工具类RateLimiter,该类基于令牌桶算法实现流量限制
+ */
+
+@Slf4j
+public class RateLimiterExample1 {
+
+    private static RateLimiter rateLimiter = RateLimiter.create(5);
+
+    public static void main(String[] args) throws Exception {
+
+        for (int index = 0; index < 100; index++) {
+            if (rateLimiter.tryAcquire(190, TimeUnit.MILLISECONDS)) {
+                handle(index);
+            }
+        }
+    }
+
+    private static void handle(int i) {
+        log.info("{}", i);
+    }
+}
+
