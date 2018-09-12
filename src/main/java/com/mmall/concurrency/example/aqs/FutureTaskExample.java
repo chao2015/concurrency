ce13e8cf1d4b6161b17558dbd26bc74ff5bc0482
chao2015 <136788909@qq.com>
2018-09-12 10:15:08 +0800

    lock

---
+++ src/main/java/com/mmall/concurrency/example/aqs/FutureTaskExample.java
@@ -0,0 +1,33 @@
+package com.mmall.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by chao on 2018/9/12.
+ *
+ * FutureTask
+ */
+
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("do something in callable");
+                Thread.sleep(3000);
+                return "Done";
+            }
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result: {}", result);
+    }
+}
---
+++ src/main/java/com/mmall/concurrency/example/lock/LockExample6.java
@@ -0,0 +1,47 @@
+package com.mmall.concurrency.example.lock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by chao on 2018/9/12.
+ *
+ * reentrantLock + condition
+ * aqs等待队列 + condition等待队列
+ */
+
+@Slf4j
+public class LockExample6 {
+
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+
+        new Thread(() -> {
+            try {
+                reentrantLock.lock();
+                log.info("wait signal"); // 1
+                condition.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("get signal"); // 4
+            reentrantLock.unlock();
+        }).start();
+
+        new Thread(() -> {
+            reentrantLock.lock();
+            log.info("get lock"); // 2
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            condition.signalAll();
+            log.info("send signal ~ "); // 3
+            reentrantLock.unlock();
+        }).start();
+    }
+}
